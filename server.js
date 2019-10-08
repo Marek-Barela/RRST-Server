b8effa29cf67e282d4eafb2999b91df29cfaee1e
Marek Bareła <marek.barela@wp.pl>
2019-10-08 17:44:12 +0200

    initial server setup commited, front end exported to client folder

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+const path = require("path");
+
+//Connect Database
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
